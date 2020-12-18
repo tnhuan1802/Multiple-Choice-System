77e25885ba081c7cd6d6eb7670f7e0a214d7243e
Pod Nguyen <nguyen.tran.pod@gmail.com>
2020-12-19 04:40:39 +0700

    finish quiz form and connect to mysql

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 from student import student
 app = Flask(__name__)
-#app.register_blueprint(student)
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'trannguyen121223'
+app.config['MYSQL_DB'] = 'fabric_database'
+
+mysql = MySQL(app)
 
 @app.route("/HomePage", methods = ['GET', 'POST'])
 def loggin():
@@ -11,6 +18,10 @@ def loggin():
         # info = request.form
         # userName = info['uname']
         # password = info['psw']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s)", ('TH0006969', 'Tran', 'Huan', 'Other', '158, Ly Thuong Kiet, Quan 10, Ho Chi Minh', '01234567891'))
+        mysql.connection.commit()
+        cur.close()
         return redirect('/HomePage/student')
     return render_template('login.html')
 
--- student.py
+++ student.py
@@ -11,7 +11,22 @@ def studentPage():
     if request.method == "POST":
         info = request.form
         choice = info['choice']
+        if choice == 'quiz':
+            questions = [("What is C++", "Language", "CCC", "Other"), ("What is Titanic", "HH Ship", "Italy Ship")]
+            questions = questions*10
+            anslist = genAnsList(questions)
+            return render_template('student.html', func= func, choice = choice, questions = questions, anslist = anslist)
+        if choice == 'submission':
+            return '<h1>Correct Answers: <u>'+str(info['Answer1'])+'</u></h1>'
         return render_template('student.html', func= func, choice = choice)
     return render_template('student.html', func = func)
+
+def genAnsList(questions):
+    ansl = []
+    for i in range(len(questions)):
+        ansl.append("Answer" + str(i))
+    return ansl
+
+
 
 
