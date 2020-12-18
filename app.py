c6ac92a726d2b3cf61d6516a4ef25ee18a7ddf00
tnhuan1802 <tnhuan1802@gmail.com>
2020-12-18 19:25:48 +0700

    First commit

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 from student import student
+from incharge import incharge
 app = Flask(__name__)
 #app.register_blueprint(student)
 
 @app.route("/HomePage", methods = ['GET', 'POST'])
-def loggin():
+def login():
     if request.method == "POST":
-        app.register_blueprint(student)
+        # app.register_blueprint(student)
+        app.register_blueprint(incharge)
         # info = request.form
         # userName = info['uname']
         # password = info['psw']
-        return redirect('/HomePage/student')
+        return redirect('/HomePage/incharge')
     return render_template('login.html')
 
 
