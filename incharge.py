8cd447c4815605fc0bab56f18b2111bc4ce3fba8
tnhuan1802 <tnhuan1802@gmail.com>
2020-12-25 20:24:48 +0700

    Done question

--- incharge.py
+++ incharge.py
@@ -12,10 +12,15 @@ def inChargePage():
 
 @incharge.route("/index/incharge/question", methods = ['GET', 'POST'])
 def questionPage():
-    q1 = Question('1', 'This is question 1',['A','B','C'])
-    q2 = Question('2', 'This is question 2',['A','B','C'])
-    listQuestion = [['1', 'This is question 1',['A','B','C']]]
-    print(listQuestion)
+    q1 = ('1', "This is question 1",['A','B','C','D'])
+    q2 = ('2', "This is question 2",['A','B','C'])
+    listQuestion = [q1,q2]
     return render_template('/incharge/question.html', listQuestion = listQuestion)
 
-
+@incharge.route("/index/incharge/exam", methods = ['GET', 'POST'])
+def examPage():
+    q1 = ('1', "This is question 1",['A','B','C','D'])
+    q2 = ('2', "This is question 2",['A','B','C'])
+    listQuestion = [q1,q2]
+    role = 1
+    return render_template('/incharge/exam.html', listQuestion = listQuestion, role = role)
